3a2aadd90e23ae4d6166ad1e4b9550b6be31e7af
Ryan Haynes <50088158+ryanhaynes01@users.noreply.github.com>
2021-10-04 19:53:46 +0100

    Image Testing for Display
    Learning how to load and display and image with OpenCV.

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello world")
+# imports
+import imutils
+import cv2 as cv
+
+print(cv.__version__)
+
+# load in a test image
+# get dimension information from NumPy array
+# display information
+image = cv.imread("professionalism_computer_vision/bin/images/linus.jpg")
+(height, width, depth) = image.shape
+print(f'Width = {width}, Height = {height}, Depth = {depth}') 
+
+# display image
+cv.imshow("Linus", image)
+cv.waitKey(0)
